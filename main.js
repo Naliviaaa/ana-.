5bc4f7d4bb98defb1cf949a853711718ce62cf4d
Naliviaaa <142615230+Naliviaaa@users.noreply.github.com>
2024-09-30 10:19:11 -0300

    k

--- main.js
+++ main.js
@@ -31,8 +31,4 @@
 
 let estado= ['paraná','são paulo','minas gerais','rio de janeiro','santa catarina','rio grande do sul','espirito santo','acre','alagoas','amapa','amazonas','bahia','ceara','alagoas','maranhão','mato grosso','mato grosso do sul','minas gerais','paraiba','rondonia','roraima','sergipe','tocantins','pernambuco','piaui','rio grande do norte','distrito federal']
 
-let i = 0
-white (i<10){
-    console log(i);
-    i=(+1)
-}
+
